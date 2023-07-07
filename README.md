7df1c321bca73911438c828303d4c2145e7d5a93
Israel <tekkyok1@gmail.com>
2023-07-07 19:51:43 +0100

    com

---
+++ README.md
@@ -0,0 +1,5 @@
+# deygo
+For logistics delivery project
+
+# ToDo
+implement a live request and reject using sockets
---
+++ gunicorn-cfg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+bind = '0.0.0.0:5000'
+workers = 2
+accesslog = '-'
+loglevel = 'debug'
+capture_output = True
+enable_stdio_inheritance = True
